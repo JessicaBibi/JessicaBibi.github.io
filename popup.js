07cc9c9a4308ee96ddbb4d0f40073f370aa6a2b8
Jessica <jessica.languille@gmail.com>
2018-09-27 16:39:04 +0200

    ajout popup

---
+++ popup.js
@@ -0,0 +1,19 @@
+	<script>
+
+    var first, second, third, result;
+    first = function f() {
+        alert("Execution retardée de 10 secondes.");
+    }
+
+    function g() {
+
+        setTimeout("f()",10000);
+
+    }
+
+    second = alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+    third = function RedirectionJs(argument) {
+
+        document.location.herf="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+    }
